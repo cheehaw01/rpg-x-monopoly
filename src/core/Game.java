1c716c17edb10f954f920a3e50c033867e464522
awebmekhilef <awebmekhilef@gmail.com>
2020-12-14 12:44:28 +0800

    Fix cannot leave shop bug + show player gold when in shop

--- src/core/Game.java
+++ src/core/Game.java
@@ -49,7 +49,7 @@ public class Game {
 			}
 
 			// Prevent out of bound exceptions
-			if(players.size() > 0) {
+			if (players.size() > 0) {
 				currentPlayerIndex++;
 				currentPlayerIndex = Util.loopClampInRange(currentPlayerIndex, 0, players.size());
 			}
@@ -59,7 +59,7 @@ public class Game {
 	}
 
 	private void generateTiles() {
-		boardTiles = new TileType[] {
+		boardTiles = new TileType[]{
 			TileType.START,
 			TileType.DUO_M,
 			TileType.TRI_M,
@@ -154,7 +154,7 @@ public class Game {
 	}
 
 	private void setupShop(Player player) {
-		System.out.printf("%n[Player %d] is in a shop%n", player.getId());
+		System.out.printf("%n[Player %d](%dG) is in a shop%n", player.getId(), player.Gold);
 		System.out.println("1. Exit shop");
 		System.out.println("2. Buy item");
 		System.out.println("3. Sell item");
@@ -173,7 +173,7 @@ public class Game {
 		while (choice != 1) {
 			switch (choice) {
 				case 2:
-					System.out.printf("%n[Player %d] Choose item to buy%n", player.getId());
+					System.out.printf("%n[Player %d](%dG) Choose item to buy%n", player.getId(), player.Gold);
 					System.out.println("1. Back");
 					System.out.printf("2. %s (%dG)%n", shopItems.get(0).Name, shopItems.get(0).Cost);
 					System.out.printf("3. %s (%dG)%n", shopItems.get(1).Name, shopItems.get(1).Cost);
@@ -181,17 +181,22 @@ public class Game {
 
 					int buyChoice = commandParser.readInt(new int[]{1, 2, 3, 4});
 
-					// TODO : Buying items (check if player has enough gold)
-					if (player.Gold >= shopItems.get(buyChoice-2).Cost) {
-						player.Gold -= shopItems.get(buyChoice-2).Cost;
-						player.addItem(shopItems.get(buyChoice-2), 1);
-						System.out.printf("[Player %d] bought %s for %dG. Your remaining gold is %dG.%n", player.getId(), shopItems.get(buyChoice-2).Name, shopItems.get(buyChoice-2).Cost, player.Gold);
-					} else {
+					// Return to shop menu
+					if (buyChoice == 1)
+						break;
+
+					// Item to buy
+					Item item = shopItems.get(buyChoice - 2);
+
+					if (player.Gold >= item.Cost) {
+						player.Gold -= item.Cost;
+						player.addItem(item, 1);
+						System.out.printf("%n[Player %d] bought %s for %dG. Remaining gold is %dG.%n",
+							player.getId(), item.Name, item.Cost, player.Gold);
+					}
+					else {
 						System.out.printf("[Player %d] gold is not enough%n", player.getId());
 					}
-					// Return to shop menu
-					if(buyChoice == 1)
-						break;
 
 					break;
 				case 3:
@@ -203,13 +208,13 @@ public class Game {
 					int sellChoice = commandParser.readInt(new int[]{1});
 
 					// Return to shop menu
-					if(sellChoice == 1)
+					if (sellChoice == 1)
 						break;
 
 					break;
 			}
 
-			System.out.printf("%n[Player %d] is1 in a shop%n", player.getId());
+			System.out.printf("%n[Player %d] is in a shop%n", player.getId());
 			System.out.println("1. Exit shop");
 			System.out.println("2. Buy item");
 			System.out.println("3. Sell item");
