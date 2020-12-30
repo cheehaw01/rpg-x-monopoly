c7937cd0552417a9edb55c4d666cc8f9a4ec41c7
LawrenceLCTY <64308072+LawrenceLCTY@users.noreply.github.com>
2020-12-30 14:38:45 +0800

    Update Monster.java

--- src/core/Monster.java
+++ src/core/Monster.java
@@ -8,15 +8,39 @@ public class Monster {
 	public int Defense;
 	public int Agility;
 	public int Exp;
-
+	public int Gold;
+	public int Level = 1;
+	public String Type;
+	public String[] Types = {"Tanker", "Lunatic", "Trickster"};//we'll leave fallen mage out for now
+	public int Stats[][][] = {/*Tanker*/{{100, 250, 400, 550}, {10, 20, 30, 40},{50, 60, 70, 80}, {5, 10, 15, 20}},
+							/*Lunatic*/{{50, 100, 150, 200}, {30, 50, 70, 90}, {10, 20, 30, 50}, {5, 20, 35, 50}},
+							/*Trickster*/{{50, 150, 250, 350}, {20, 30, 40, 50}, {30, 40, 50, 60}, {20, 40, 60, 80}},};
+							/* 				Health				Strength			Defense				Agility
+							By level*/
 	public Monster() {
-		// Maybe generate values based on player level
+		// Generate values and types based on player level.
+		int choiceType = Util.RandomBetween(0, 2);
+		switch (choiceType) {
+			case 0:
+				Type = "Tanker";
+				break;
+			
+			case 1:
+				Type = "Lunatic";
+				break;
 
-		Health = Util.RandomBetween(5, 15);
-		Strength = Util.RandomBetween(2, 4);
-		Defense = Util.RandomBetween(1, 3);
-		Agility = Util.RandomBetween(1, 3);
+			case 2:
+				Type = "Trickster";
+				break;
+		}
+		//TODO: generate level based on player level.
 
-		Exp = Util.RandomBetween(25, 50);
+		Gold = Util.RandomBetween(10, 50) * Level;
+		Exp = Util.RandomBetween(10, 30) * Level;
+
+		Health = Stats[choiceType][0][Level - 1];
+		Strength = Stats[choiceType][1][Level - 1];
+		Defense = Stats[choiceType][2][Level - 1];
+		Agility = Stats[choiceType][3][Level - 1];
 	}
 }
