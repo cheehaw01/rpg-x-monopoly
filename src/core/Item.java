91d0fab9ac285d2d1335085a5f2f052afeed702e
cheehaw01 <75060656+cheehaw01@users.noreply.github.com>
2021-01-02 22:39:56 +0800

    Update Item.java
    Type

--- src/core/Item.java
+++ src/core/Item.java
@@ -4,15 +4,17 @@ package core;
 public class Item {
 	public String Name;
 	public int Cost;
+	public String Type;
 
 	public final int HealBonus; // Does not immediately add to health. Instead is added when item is used in battle
 	public final int StrengthBonus;
 	public final int DefenseBonus;
 	public final int AgilityBonus;
 
-	public Item(String name, int cost, int healBonus, int strengthBonus, int defenseBonus, int agilityBonus) {
+	public Item(String name, int cost, String type, int healBonus, int strengthBonus, int defenseBonus, int agilityBonus) {
 		Cost = cost;
 		Name = name;
+		Type = type;
 		HealBonus = healBonus;
 		StrengthBonus = strengthBonus;
 		DefenseBonus = defenseBonus;
